5607dea3a46465fe29be8abdcd4d06757cce713b
Saurav Mishra <54716913+sauravmishra1017@users.noreply.github.com>
2019-08-30 23:48:10 +0530

    Rename CodeChef Q1 Solution C++.txt to CodeChef Q1 Solution C++.cpp

---
+++ Q1 - PIZZA BET/CodeChef Q1 Solution C++.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int m,c,p1=0,p2=0,i=0,ate;
+	cin>>m>>c;
+	int slice=(m/c)*6;
+	while(slice>0)
+	{
+	    cin>>ate;
+	    if(i%2==0)
+	        p1+=ate;
+	    else
+	        p2+=ate;
+	    slice-=ate;
+        i++;
+	}
+	if(p1>p2)
+		cout<<"Arjun";
+	else
+		cout<<"Sara";
+	return 0;
+}

7d70e72dd0e2302fc5676431481c0213a65aba43
Saurav Mishra <54716913+sauravmishra1017@users.noreply.github.com>
2019-08-30 23:52:01 +0530

    Rename CodeChef Q5 Solution C++ 2.txt to CodeChef Q5 Solution C++ 2.cpp

---
+++ Q5 - PSURGCL/CodeChef Q5 Solution C++ 2.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,l,chr,ct;
+    char r,am[6][6];
+    cin>>n;
+    while(n-->0)
+    {
+        cin>>r>>l;
+        while(l-->0)
+        {
+            cin>>ct;
+            int row=ct/10;
+            int column=ct%10;
+            cout<<(char)(97+((row-1)*5)+column);
+        }
+        cout<<endl;
+    }
+    return 0;
+}

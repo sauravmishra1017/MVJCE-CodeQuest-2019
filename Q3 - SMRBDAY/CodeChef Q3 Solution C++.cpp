c33ed81ffaf3bff2e89dec67fb009efd4443655a
Saurav Mishra <54716913+sauravmishra1017@users.noreply.github.com>
2019-08-30 23:49:00 +0530

    Rename CodeChef Q3 Solution C++.txt to CodeChef Q3 Solution C++.cpp

---
+++ Q3 - SMRBDAY/CodeChef Q3 Solution C++.cpp
@@ -0,0 +1,26 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,p,count,sum;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    count=0,sum=0;
+	    cin>>p;
+	    for(int j=p;j>0;j--)
+	    {
+	        if((j & (j-1))==0)  //to check where j is a power of 2
+	        {
+	            if((sum+j)>p)
+	                continue;
+	            sum+=j;
+	            count++;
+	        }
+	        if(sum==p)
+	            break;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}

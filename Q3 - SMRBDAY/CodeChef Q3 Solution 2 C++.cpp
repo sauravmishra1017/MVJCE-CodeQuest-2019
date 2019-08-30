0bd27d1ec4684c17958d6646cbaaaeaa068921cd
Saurav Mishra <54716913+sauravmishra1017@users.noreply.github.com>
2019-08-30 23:49:21 +0530

    Rename CodeChef Q3 Solution 2 C++.txt to CodeChef Q3 Solution 2 C++.cpp

---
+++ Q3 - SMRBDAY/CodeChef Q3 Solution 2 C++.cpp
@@ -0,0 +1,20 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,p,count;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    count=0;
+	    cin>>p;
+        while(p!=0)
+        {
+	        if(p%2!=0)
+	            count++;
+	        p/=2;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}

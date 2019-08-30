508a484301ed87e0ba6b38710827444f629e9e2a
Saurav Mishra <54716913+sauravmishra1017@users.noreply.github.com>
2019-08-30 23:50:02 +0530

    Rename CodeChef Q4 Solution C++.txt to CodeChef Q4 Solution C++.cpp

---
+++ Q4 - AMCIPHER/CodeChef Q4 Solution C++.cpp
@@ -0,0 +1,34 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,l,chr,ct;
+    char r,am[6][6];
+    cin>>n;
+    while(n-->0)
+    {
+        chr=97;
+        cin>>r;
+        for(int i=1;i<6;i++)
+        {
+            for(int j=1;j<6;j++)
+            {
+                if(r==(char)chr)
+                    chr++;
+                am[i][j]=chr;
+                //cout<<am[i][j]<<i<<j<<" ";
+                chr++;
+            }
+        }
+        cin>>l;
+        while(l-->0)
+        {
+            cin>>ct;
+            int row=ct/10;
+            int column=ct%10;
+            cout<<am[row][column];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
